1458266749d381c0501dce8c6aaa547190a094ff
Anjanee Wijewardana <anjaneew@gmail.com>
2025-05-17 11:52:57 +0300

    Initial commit

---
+++ src/lesson1/Main.java
@@ -0,0 +1,38 @@
+package lesson1;
+
+public class Main {
+    public static void main(String[] args){
+        System.out.print("I like pizza.");//just print
+        System.out.println("It's cheesy.");//print in new line
+        System.out.println("and strawberry ice cream");
+        System.out.print("But strawberries with ice cream\n");//new line \n
+        System.out.print("wasn't that great!");
+
+        //this is a comment
+        /* This is a multi
+           line comment
+           dha!
+         */
+
+        //sout + TAB =  System.out.println();
+        System.out.println("\nIntelliJ Homework");
+
+        //homework
+        System.out.println("To BroCode, With Thanks");
+
+        System.out.println("In a sea of code, where many drift,");
+        System.out.println("you gave our learning path a lift.");
+        System.out.println("With every line, concise and clear,");
+        System.out.println("you brought the world of code so near.");
+
+        System.out.println("No fluff, no noise, just skills that grow,");
+        System.out.println("from webdev to Java’s lessons flow.");
+        System.out.println("You teach with heart, you teach with style,");
+        System.out.println("and make each hard thing worth the while.");
+
+        System.out.println("For every free and golden thread,");
+        System.out.println("For all the lines our fingers’ve read,");
+        System.out.println("We thank you, Bro, for what you do—");
+        System.out.println("This journey’s bright because of you.");
+    }
+}
---
+++ src/lesson2/Main.java
@@ -0,0 +1,85 @@
+package lesson2;
+
+public class Main {
+    public static void main(String[] args){
+
+        //Variables
+
+        /*
+        Variables               = Primitive            VS   Reference
+        reusable code container   simple value              memory address (stack)
+                                  in memory(stack)          that points to (heap)
+
+                                  int                       string
+                                  double                    array
+                                  char                      object
+                                  boolean
+        */
+
+        /*
+        Creating a variable
+            1. declaration  int age;
+            2. assignment
+         */
+
+        // together
+
+        //integer
+        int age = 21;
+        int year = 2025;
+        int quantity = 1;
+        System.out.println(age);
+        System.out.println(year);
+        System.out.println(quantity);
+
+        System.out.println("The year is " + year);
+
+        //double
+        double price = 19.99;
+        double gpa = 3.2;
+        double temperature = -12.5;
+
+        System.out.println("$"+price + " my gpa is "+ gpa);
+        //even if double is 19 it will print as 19.0
+        System.out.println("The temperature is " + temperature);
+
+
+        //char
+        char grade = 'A';
+        char symbol = '!';
+        char currency = '$';
+
+        System.out.println("My grade is "+ grade + ".\nWHY "+ symbol +
+                ".\nThe currency is "+ currency);
+
+        //boolean
+        boolean isStudent = true;
+        boolean forSale = false;
+        boolean isOnline = true;
+
+        System.out.println(isStudent);
+
+        if(isOnline){
+            System.out.println("The user is online.");
+        }
+        else{
+            System.out.println("The user is not online.");
+        }
+
+        //String
+        String name = "Anjanee";
+        String food = "cashew";
+        String email = "bookwarm@gmail.com";
+        String car = "Maclaren";
+
+        System.out.println("My name is "+ name);
+        System.out.println("my choice is " + year + " " + car);
+
+        //homework
+        String hobby = "watching movies";
+        int count = 15;
+        double bankBalance = 19.09;
+        char degreeSymbol = '°';
+        boolean isEmployed = false;
+    }
+}
