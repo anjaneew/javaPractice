c85544f83c5cf0ed8bf7c42138f6a2bd7bd3e0ab
Anjanee Wijewardana <anjaneew@gmail.com>
2025-05-20 06:46:30 +0300

    5 Arithmatic Operators

---
+++ src/lesson5/Main.java
@@ -0,0 +1,28 @@
+package lesson5;
+
+public class Main {
+
+    public static void main(String[] args){
+
+        //Arithmatic Operators
+
+        int x = 10;
+        int y = 2;
+        int z;
+
+        z = x + y; // - * / %
+
+        //Augmented Assignment Operators
+
+        x += y;
+
+        //increment and decrement
+        x++;
+
+        //Order of operations [PEMDAS]
+        //please excuse my dear aunt sally
+
+        double result =   3 + 4 * ( 7 - 5 ) / 2.0;
+        System.out.println(result);
+    }
+}
